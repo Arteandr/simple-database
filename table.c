06d7f19d0a29033d08c064d17094943741fe8a74
arteandr <artedamatik@gmail.com>
2022-09-09 21:31:56 +0300

    Add serialize and deserialize functions

--- table.c
+++ table.c
@@ -6,3 +6,8 @@ void serialize_row(Row *src, void *dest) {
   memcpy(dest + EMAIL_OFFSET, &(src->email), EMAIL_SIZE);
 }
 
+void deserialize_row(void *src, Row *dest) {
+  memcpy(&(dest->id), src + ID_OFFSET, ID_SIZE);
+  memcpy(&(dest->username), src + USERNAME_OFFSET, USERNAME_SIZE);
+  memcpy(&(dest->email), src + EMAIL_OFFSET, EMAIL_SIZE);
+}
